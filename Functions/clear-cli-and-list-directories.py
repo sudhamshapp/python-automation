cab0e4ee95dcf6d7af6e5b23a6c632cb37a0de94
sudhamsh <sudhamsh1996@gmail.com>
2023-10-28 15:19:54 +0530

    added fewer functions

--- Functions/clear-cli-and-list-directories.py
+++ Functions/clear-cli-and-list-directories.py
@@ -10,10 +10,11 @@ def mycode(cmd1, cmd2):
     print("finding the list of directories and files")
     time.sleep(2)
     os.system(cmd2)
+    return None
 if platform.system() == "Windows":
     mycode("cls", "dir")
 else:
     mycode("clear", "ls -ltra")
 
-# platform_name = platform.system()
+# platform_name = platform.system() - here platform is the module and system is the method, method can be null() as well as accepts the arguments(a, b)
 # print(platform_name)    

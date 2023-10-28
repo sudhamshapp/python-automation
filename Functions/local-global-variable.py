4c411dbeed046d1bf520310ab46c52b7301090db
sudhamsh <sudhamsh1996@gmail.com>
2023-10-28 19:12:35 +0530

    added local, global and script dependant files

---
+++ Functions/local-global-variable.py
@@ -0,0 +1,19 @@
+def sudhamsh():
+    x = 2
+    print(x)
+    mars(x) # while passing it's argument
+    return None
+
+def mars(y): # while calling it's argument
+    y = 3
+    print(y)
+    mars2(y)
+    return None
+    print(y)
+    return None
+def main():
+    # global x
+    x = 5
+    sudhamsh()
+    return None
+main()

81b18c8c291973f53903c01f2d2edca3e8ee8f55
sudhamsh <sudhamsh1996@gmail.com>
2023-10-29 14:26:21 +0530

    now we can run the python like this as well

---
+++ running-python-on-linux/example.py
@@ -0,0 +1,5 @@
+# on linux cli just enter the command which python3 - it's gonna spit the path(/usr/local/bin/python3)
+#!/usr/local/bin/python3
+print("hello world")
+# then chmod +x <filename>
+# then ./<filename>

51ffc82a95310d7d240f768ab48c76e42d3f828f
sudhamsh <sudhamsh1996@gmail.com>
2023-10-31 18:16:18 +0530

    added the while loop

---
+++ loops/while-loop.py
@@ -0,0 +1,25 @@
+'''
+for each in range(10):
+    print("sudhamsh's pet name is mars")
+'''
+'''
+while True: #this is gonna run script infinite times
+    print("sudhamsh's pet name is mars")
+'''
+'''
+import time
+while True:
+    print("monitoring the file-system")
+    time.sleep(2)
+'''
+
+
+value = 2 # initialized a variable
+while True:
+    print(value) # mo need here, but checks the stuff
+    value += 1
+    if value == 10:
+        break
+    else:
+        continue
+print(value)    

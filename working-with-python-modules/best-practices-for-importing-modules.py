41ec8158149d345d736b8ed06b140de5a712a858
sudhamsh <sudhamsh1996@gmail.com>
2023-10-31 11:08:24 +0530

    added the modules folder

---
+++ working-with-python-modules/best-practices-for-importing-modules.py
@@ -0,0 +1,33 @@
+'''
+import math
+print(math.pi)
+'''
+
+'''
+from math import *
+print(pi)
+print(pow(4, 3))
+'''
+
+'''
+from math import pi, pow
+print(pi)
+print(pow(5, 3))
+'''
+
+'''
+# using the desired alias names
+import math as m
+print(m.pi)
+print(m.pow(5, 3))
+'''
+
+'''
+import math, sys, subprocess, platform, os
+'''
+
+import math
+import sys
+import subprocess
+import platform
+import os
---
+++ working-with-python-modules/script2.py
@@ -0,0 +1,10 @@
+# module is a file containing python definitions and statements. Function  always needs inputs
+import script1
+print(script1.my_value)# calling from the script1 module
+
+# default module
+import math
+print(math.pi)
+print(math.pow(4, 3))
+print(dir(math))
+print(help(math))

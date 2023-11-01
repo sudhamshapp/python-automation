09381518bf36a0f72037abd6aa7b9ba44769e36a
sudhamsh <sudhamsh1996@gmail.com>
2023-11-01 13:15:57 +0530

    added, how to find a java-version on host

--- sub-process/find-java-version.py
+++ sub-process/find-java-version.py
@@ -7,8 +7,13 @@ o, e = sp.communicate()
 if rc == 0:
     if bool(o) == True: # I think this block isn't necessary  
         print(o)
+    '''
     if bool(o) == False and bool(e) == True:
         print(e.splitlines()[0].split()[2].strip("\""))    
+    '''
+    if bool(o) == False: # instead of these lines we can use and operator as above
+        if bool(e) == True:
+            print(e.splitlines()[0].split()[2].strip("\""))
 else:
     print("java is not installed")
     print(e)    
